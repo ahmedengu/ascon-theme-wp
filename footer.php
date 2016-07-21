0ac778561061f176e3dbea5ccdd02f41603217b4
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-07-21 21:52:59 +0200

    --

--- footer.php
+++ footer.php
@@ -1,15 +1,14 @@
-
-        </div>
-        <div class="copyright-bar bg-black">
-            <div class="container">
-                <?php if( get_theme_mod( 'blocks_copyright_1_text_left' ) ) : ?>
-                    <p class="pull-left small"><?php echo get_theme_mod( 'blocks_copyright_1_text_left', __( '© ascon', 'ascontheme' ) ); ?></p>
-                <?php endif; ?>
-                <?php if( get_theme_mod( 'blocks_copyright_1_text_right' ) ) : ?>
-                    <p class="pull-right small"><?php echo get_theme_mod( 'blocks_copyright_1_text_right', '<a href="https://github.com/ahmedengu/ascon-theme-wp">ascon-theme</a>' ); ?></p>
-                <?php endif; ?>
-            </div>
-        </div>                  
-        <?php wp_footer(); ?>
-    </body>     
+</div>
+<div class="copyright-bar bg-black">
+    <div class="container">
+        <?php if (get_theme_mod('blocks_copyright_1_text_left')) : ?>
+            <p class="pull-left small"><?php echo get_theme_mod('blocks_copyright_1_text_left', __('© ascon', 'ascontheme')); ?></p>
+        <?php endif; ?>
+        <?php if (get_theme_mod('blocks_copyright_1_text_right')) : ?>
+            <p class="pull-right small"><?php echo get_theme_mod('blocks_copyright_1_text_right', '<a href="https://github.com/ahmedengu/ascon-theme-wp">ascon-theme</a>'); ?></p>
+        <?php endif; ?>
+    </div>
+</div>
+<?php wp_footer(); ?>
+</body>
 </html>
